17818cde48664f157ce08940b19ecbd07ad6e6ce
NiteChill <puissantachille@gmail.com>
2026-03-21 16:52:57 +0100

    feat(Localisation): complete localisation implementation

--- src/Localisation.java
+++ src/Localisation.java
@@ -1,17 +1,36 @@
 public class Localisation {
-    private long id;
-    private double altitude;
 
-    public long getId() {
-        return id;
-    }
+  private final long id;
+  private final double altitude;
+  private final double longitude;
+  private final double latitude;
+  private final String nom;
 
-    public double getAltitude() {
-        return altitude;
-    }
+  public Localisation(long id, double altitude, double longitude, double latitude, String nom) {
+    this.id = id;
+    this.altitude = altitude;
+    this.longitude = longitude;
+    this.latitude = latitude;
+    this.nom = nom;
+  }
 
-    public Localisation(long id, double altitude) {
-        this.id = id;
-        this.altitude = altitude;
-    }
+  public long getId() {
+    return id;
+  }
+
+  public double getAltitude() {
+    return altitude;
+  }
+
+  public double getLongitude() {
+    return longitude;
+  }
+
+  public double getLatitude() {
+    return latitude;
+  }
+
+  public String getNom() {
+    return nom;
+  }
 }
