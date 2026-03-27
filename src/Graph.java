fda196602c9b2cb06e43dd626ff4e146edca8567
alexane-dubois-vinci <alexane.dubois@student.vinci.be>
2026-03-27 15:47:12 +0100

    feat: add find shortest path BFS algorithm

--- src/Graph.java
+++ src/Graph.java
@@ -1,8 +1,10 @@
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
@@ -25,12 +27,12 @@ public class Graph {
   }
 
   public Localisation[] determinerZoneInondee(long[] idsOrigin, double epsilon) {
-    Queue<Localisation>  aVisiter = new ArrayDeque<>();
+    Queue<Localisation> aVisiter = new ArrayDeque<>();
     Set<Long> dejaInondes = new HashSet<>();
     List<Localisation> ordreInondation = new ArrayList<>();
 
     // Initialisation avec les points de départs de l'inondation
-    for (long id: idsOrigin){
+    for (long id : idsOrigin) {
       Localisation depart = this.localisations.get(id);
       aVisiter.add(depart);
       dejaInondes.add(id);
@@ -38,21 +40,19 @@ public class Graph {
     }
 
     // Boucle du BFS
-    while (!aVisiter.isEmpty()){
+    while (!aVisiter.isEmpty()) {
       Localisation noeudCourant = aVisiter.poll();
       List<Arc> arcsSortants = this.listeAdjacence.get(noeudCourant.getId());
 
-      if (arcsSortants != null){
-        for (Arc arc: arcsSortants){
-          Localisation voisin = arc.getPointArrivee();
+      for (Arc arc : arcsSortants) {
+        Localisation voisin = arc.getPointArrivee();
 
-          boolean penteFavorable = voisin.getAltitude() < (noeudCourant.getAltitude() + epsilon);
+        boolean penteFavorable = voisin.getAltitude() <= (noeudCourant.getAltitude() + epsilon);
 
-          if (penteFavorable && !dejaInondes.contains(voisin.getId())){
-            aVisiter.add(voisin);
-            dejaInondes.add(voisin.getId());
-            ordreInondation.add(voisin);
-          }
+        if (penteFavorable && !dejaInondes.contains(voisin.getId())) {
+          aVisiter.add(voisin);
+          dejaInondes.add(voisin.getId());
+          ordreInondation.add(voisin);
         }
       }
     }
@@ -62,7 +62,60 @@ public class Graph {
   public Deque<Localisation> trouverCheminLePlusCourtPourContournerLaZoneInondee(long idOrigin,
       long idDestination, Localisation[] floodedZone) {
     //TODO
-    return null;
+
+    // Plus opti de vérifier dans un set si le noeud fait partie de la zone inondée.
+    // On doit parcourir la liste dans sa totalité uniquement lors de la transformation en set
+    // plutôt qu'à chaque vérification dans le BFS.
+    Set<Long> zoneInondeSet = new HashSet<>();
+    for (Localisation loc : floodedZone) {
+        zoneInondeSet.add(loc.getId());
+    }
+
+    Queue<Localisation> aVisiter = new ArrayDeque<>();
+    Set<Long> dejaVisites = new HashSet<>();
+    Map<Long, Localisation> cheminEmprunte = new HashMap<>();
+
+    Localisation depart = this.localisations.get(idOrigin);
+    aVisiter.add(depart);
+    dejaVisites.add(idOrigin);
+
+    // BFS
+    while (!aVisiter.isEmpty()) {
+      Localisation noeudCourant = aVisiter.poll();
+
+      // Fin du parcours
+      if (noeudCourant.getId() == idDestination) {
+        return reconstruireCheminEmprunte(cheminEmprunte, depart, noeudCourant);
+      }
+
+      // Arrivée non atteinte -> on continue la recherche
+      List<Arc> arcsSortants = this.listeAdjacence.get(noeudCourant.getId());
+      for (Arc arc : arcsSortants){
+        Long idVoisin = arc.getPointArrivee().getId();
+
+        if (!dejaVisites.contains(idVoisin) && !zoneInondeSet.contains(idVoisin)){
+          aVisiter.add(arc.getPointArrivee());
+          dejaVisites.add(idVoisin);
+          cheminEmprunte.put(idVoisin, noeudCourant);
+        }
+      }
+    }
+
+    return new LinkedList<>();
+  }
+
+  private Deque<Localisation> reconstruireCheminEmprunte(Map<Long, Localisation> cheminEmprunte,
+      Localisation depart, Localisation arrivee) {
+      Deque<Localisation> chemin = new LinkedList<>();
+      Localisation noeud = arrivee;
+
+      while ((noeud != null) && noeud.getId() != depart.getId()){
+        chemin.addFirst(noeud);
+        noeud = cheminEmprunte.get(noeud.getId());
+      }
+
+      chemin.addFirst(depart);
+      return chemin;
   }
 
   public Map<Localisation, Double> determinerChronologieDeLaCrue(long[] idsOrigin,
