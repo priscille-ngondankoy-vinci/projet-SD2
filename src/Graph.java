f3352d45e76e6dfa4e2900cae95e1d65af8b8795
NiteChill <puissantachille@gmail.com>
2026-03-29 15:14:09 +0200

    feat(variables): make variables names easier to understand for code review

--- src/Graph.java
+++ src/Graph.java
@@ -5,308 +5,302 @@ public class Graph {
 
   private Map<Long, List<Arc>> listeAdjacence;
   private Map<Long, Localisation> localisations;
-    private Localisation[] noeuds;
-    private Map<Long, Integer> idToIndex;
-    private List<Integer>[] adj;
-    private double[][] dist;
-
-    //ATTRIBUT ?
-  //TODO
+  private Localisation[] tableauNoeuds;
+  private Map<Long, Integer> correspondanceIdVersIndice;
+  private List<Integer>[] listeVoisins;
+  private double[][] distancesArcs;
 
   public Graph(String localisations, String roads) {
     this.listeAdjacence = new HashMap<>();
     this.localisations = new HashMap<>();
 
-    Lecteur l = new Lecteur(this.listeAdjacence, this.localisations);
-    l.chargerLocalisations(localisations);
-    l.chargerArcs(roads);
+    Lecteur lecteur = new Lecteur(this.listeAdjacence, this.localisations);
+    lecteur.chargerLocalisations(localisations);
+    lecteur.chargerArcs(roads);
     construireStructureOptimisee();
   }
-    private void construireStructureOptimisee() {
 
-        int n = localisations.size();
+  private void construireStructureOptimisee() {
 
-        noeuds = new Localisation[n];
-        idToIndex = new HashMap<>();
+    int nombreNoeuds = localisations.size();
 
-        int idx = 0;
-        for (Localisation loc : localisations.values()) {
-            noeuds[idx] = loc;
-            idToIndex.put(loc.getId(), idx);
-            idx++;
-        }
+    tableauNoeuds = new Localisation[nombreNoeuds];
+    correspondanceIdVersIndice = new HashMap<>();
 
-        adj = new ArrayList[n];
-        for (int i = 0; i < n; i++) adj[i] = new ArrayList<>();
-
-        // Conversion des ids de type long en int
-        for (Map.Entry<Long, List<Arc>> entry : listeAdjacence.entrySet()) {
-            int from = idToIndex.get(entry.getKey());
-            for (Arc arc : entry.getValue()) {
-                int to = idToIndex.get(arc.getPointArrivee().getId());
-                adj[from].add(to);
-            }
-        }
-        dist = new double[n][];
-        for (int u = 0; u < n; u++) {
-            List<Arc> arcs = listeAdjacence.get(noeuds[u].getId());
-            dist[u] = new double[arcs.size()];
-            for (int i = 0; i < arcs.size(); i++) {
-                dist[u][i] = arcs.get(i).getDistance();
-            }
-        }
+    int indice = 0;
+    for (Localisation localisation : localisations.values()) {
+      tableauNoeuds[indice] = localisation;
+      correspondanceIdVersIndice.put(localisation.getId(), indice);
+      indice++;
     }
 
-
-
-    public Localisation[] determinerZoneInondee(long[] idsOrigin, double epsilon) {
-    Queue<Localisation> aVisiter = new ArrayDeque<>();
-    Set<Long> dejaInondes = new HashSet<>();
-    List<Localisation> ordreInondation = new ArrayList<>();
-
-    for (long id : idsOrigin) {
-      Localisation depart = this.localisations.get(id);
-      aVisiter.add(depart);
-      dejaInondes.add(id);
-      ordreInondation.add(depart);
+    listeVoisins = new ArrayList[nombreNoeuds];
+    for (int i = 0; i < nombreNoeuds; i++) {
+      listeVoisins[i] = new ArrayList<>();
     }
 
-    while (!aVisiter.isEmpty()) {
-      Localisation noeudCourant = aVisiter.poll();
+    // Conversion des ids de type long en int
+    for (Map.Entry<Long, List<Arc>> entree : listeAdjacence.entrySet()) {
+      int indiceDepart = correspondanceIdVersIndice.get(entree.getKey());
+      for (Arc arc : entree.getValue()) {
+        int indiceArrivee = correspondanceIdVersIndice.get(arc.getPointArrivee().getId());
+        listeVoisins[indiceDepart].add(indiceArrivee);
+      }
+    }
+    distancesArcs = new double[nombreNoeuds][];
+    for (int indiceNoeud = 0; indiceNoeud < nombreNoeuds; indiceNoeud++) {
+      List<Arc> arcsDuNoeud = listeAdjacence.get(tableauNoeuds[indiceNoeud].getId());
+      distancesArcs[indiceNoeud] = new double[arcsDuNoeud.size()];
+      for (int i = 0; i < arcsDuNoeud.size(); i++) {
+        distancesArcs[indiceNoeud][i] = arcsDuNoeud.get(i).getDistance();
+      }
+    }
+  }
+
+  public Localisation[] determinerZoneInondee(long[] idsOrigin, double epsilon) {
+    Queue<Localisation> fileNoeudsAVisiter = new ArrayDeque<>();
+    Set<Long> ensembleIdsDejaInondes = new HashSet<>();
+    List<Localisation> listeOrdreInondation = new ArrayList<>();
+
+    for (long identifiant : idsOrigin) {
+      Localisation pointDepart = this.localisations.get(identifiant);
+      fileNoeudsAVisiter.add(pointDepart);
+      ensembleIdsDejaInondes.add(identifiant);
+      listeOrdreInondation.add(pointDepart);
+    }
+
+    while (!fileNoeudsAVisiter.isEmpty()) {
+      Localisation noeudCourant = fileNoeudsAVisiter.poll();
       List<Arc> arcsSortants = this.listeAdjacence.get(noeudCourant.getId());
 
-      for (Arc arc : arcsSortants) {
-        Localisation voisin = arc.getPointArrivee();
+      for (Arc arcCourant : arcsSortants) {
+        Localisation noeudVoisin = arcCourant.getPointArrivee();
 
-        boolean penteFavorable = voisin.getAltitude() <= (noeudCourant.getAltitude() + epsilon);
+        boolean penteFavorable =
+            noeudVoisin.getAltitude() <= (noeudCourant.getAltitude() + epsilon);
 
-        if (penteFavorable && !dejaInondes.contains(voisin.getId())) {
-          aVisiter.add(voisin);
-          dejaInondes.add(voisin.getId());
-          ordreInondation.add(voisin);
+        if (penteFavorable && !ensembleIdsDejaInondes.contains(noeudVoisin.getId())) {
+          fileNoeudsAVisiter.add(noeudVoisin);
+          ensembleIdsDejaInondes.add(noeudVoisin.getId());
+          listeOrdreInondation.add(noeudVoisin);
         }
       }
     }
-    return ordreInondation.toArray(new Localisation[0]);
+    return listeOrdreInondation.toArray(new Localisation[0]);
   }
 
-    public Deque<Localisation> trouverCheminLePlusCourtPourContournerLaZoneInondee(
-            long idOrigin,
-            long idDestination,
-            Localisation[] floodedZone) {
+  public Deque<Localisation> trouverCheminLePlusCourtPourContournerLaZoneInondee(
+      long idOrigin,
+      long idDestination,
+      Localisation[] floodedZone) {
 
-        Set<Long> zoneInondeSet = new HashSet<>();
-        for (Localisation loc : floodedZone) {
-            zoneInondeSet.add(loc.getId());
-        }
-
-        Queue<Localisation> aVisiter = new ArrayDeque<>();
-        Set<Long> dejaVisites = new HashSet<>();
-        Map<Long, Localisation> parent = new HashMap<>();
-
-        Localisation depart = this.localisations.get(idOrigin);
-        Localisation arrivee = this.localisations.get(idDestination);
-
-        aVisiter.add(depart);
-        dejaVisites.add(idOrigin);
-
-        // BFS
-        while (!aVisiter.isEmpty()) {
-            Localisation courant = aVisiter.poll();
-
-            if (courant.getId() == idDestination) {
-                return reconstruireCheminEmprunte(parent, depart, arrivee);
-            }
-
-            List<Arc> arcsSortants = this.listeAdjacence.get(courant.getId());
-            if (arcsSortants == null) continue;
-
-            for (Arc arc : arcsSortants) {
-                Localisation voisin = arc.getPointArrivee();
-                long idVoisin = voisin.getId();
-
-                if (!dejaVisites.contains(idVoisin) && !zoneInondeSet.contains(idVoisin)) {
-                    dejaVisites.add(idVoisin);
-                    parent.put(idVoisin, courant);
-                    aVisiter.add(voisin);
-                }
-            }
-        }
-
-        return new ArrayDeque<>();
+    Set<Long> ensembleIdsZoneInondee = new HashSet<>();
+    for (Localisation localisation : floodedZone) {
+      ensembleIdsZoneInondee.add(localisation.getId());
     }
 
+    Queue<Localisation> fileNoeudsAVisiter = new ArrayDeque<>();
+    Set<Long> ensembleIdsDejaVisites = new HashSet<>();
+    Map<Long, Localisation> predecesseur = new HashMap<>();
 
-    private Deque<Localisation> reconstruireCheminEmprunte(
-            Map<Long, Localisation> parent,
-            Localisation depart,
-            Localisation arrivee) {
+    Localisation pointDepart = this.localisations.get(idOrigin);
+    Localisation pointArrivee = this.localisations.get(idDestination);
 
-        Deque<Localisation> chemin = new ArrayDeque<>();
-        Localisation courant = arrivee;
+    fileNoeudsAVisiter.add(pointDepart);
+    ensembleIdsDejaVisites.add(idOrigin);
 
-        while (courant != null && courant != depart) {
-            chemin.addFirst(courant);
-            courant = parent.get(courant.getId());
+    // BFS
+    while (!fileNoeudsAVisiter.isEmpty()) {
+      Localisation noeudCourant = fileNoeudsAVisiter.poll();
+
+      if (noeudCourant.getId() == idDestination) {
+        return reconstruireCheminEmprunte(predecesseur, pointDepart, pointArrivee);
+      }
+
+      List<Arc> arcsSortants = this.listeAdjacence.get(noeudCourant.getId());
+      if (arcsSortants == null) {
+        continue;
+      }
+
+      for (Arc arcCourant : arcsSortants) {
+        Localisation noeudVoisin = arcCourant.getPointArrivee();
+        long idDuVoisin = noeudVoisin.getId();
+
+        if (!ensembleIdsDejaVisites.contains(idDuVoisin) && !ensembleIdsZoneInondee.contains(
+            idDuVoisin)) {
+          ensembleIdsDejaVisites.add(idDuVoisin);
+          predecesseur.put(idDuVoisin, noeudCourant);
+          fileNoeudsAVisiter.add(noeudVoisin);
         }
-
-        chemin.addFirst(depart);
-        return chemin;
+      }
     }
 
-    public Map<Localisation, Double> determinerChronologieDeLaCrue(
-            long[] idsOrigin,
-            double vWaterInit,
-            double k) {
+    return new ArrayDeque<>();
+  }
 
-        int n = noeuds.length;
+  private Deque<Localisation> reconstruireCheminEmprunte(
+      Map<Long, Localisation> parent,
+      Localisation depart,
+      Localisation arrivee) {
 
-        double[] temps = new double[n];
-        Arrays.fill(temps, Double.POSITIVE_INFINITY);
+    Deque<Localisation> cheminReconstruit = new ArrayDeque<>();
+    Localisation noeudCourant = arrivee;
 
-        double[] vitesse = new double[n];
-        Arrays.fill(vitesse, 0);
-
-        // Dijkstra
-        PriorityQueue<Integer> pq =
-                new PriorityQueue<>(Comparator.comparingDouble(i -> temps[i]));
-
-        for (long id : idsOrigin) {
-            int idx = idToIndex.get(id);
-            temps[idx] = 0.0;
-            vitesse[idx] = vWaterInit;
-            pq.add(idx);
-        }
-
-        while (!pq.isEmpty()) {
-            int u = pq.poll();
-
-            List<Arc> arcs = listeAdjacence.get(noeuds[u].getId());
-
-            for (Arc arc : arcs) {
-
-                int v = idToIndex.get(arc.getPointArrivee().getId());
-                double dist = arc.getDistance();
-
-                // Calcul de la pente
-
-                double pente = (noeuds[u].getAltitude() - noeuds[v].getAltitude()) / dist;
-                // Calcul de la vitesse
-                double newVitesse = vitesse[u] + k * pente;
-                // Si vitesse négative ou nulle, l'eau s'arrête
-                if (newVitesse <= 0) continue;
-
-                double tempsArc = dist / newVitesse;
-
-                double nouveauTemps = temps[u] + tempsArc;
-
-                if (nouveauTemps < temps[v]) {
-                    temps[v] = nouveauTemps;
-                    vitesse[v] = newVitesse;
-                    pq.add(v);
-                }
-            }
-        }
-
-        Localisation[] copie = Arrays.copyOf(noeuds, n);
-        // Tri  par temps pour la Map
-        Arrays.sort(copie, Comparator.comparingDouble(
-                loc -> temps[idToIndex.get(loc.getId())]
-        ));
-
-        Map<Localisation, Double> resultat = new LinkedHashMap<>();
-
-        for (Localisation loc : copie) {
-            double t = temps[idToIndex.get(loc.getId())];
-            if (t < Double.POSITIVE_INFINITY) {
-                resultat.put(loc, t);
-            }
-        }
-
-        return resultat;
+    while (noeudCourant != null && noeudCourant != depart) {
+      cheminReconstruit.addFirst(noeudCourant);
+      noeudCourant = parent.get(noeudCourant.getId());
     }
 
+    cheminReconstruit.addFirst(depart);
+    return cheminReconstruit;
+  }
 
+  public Map<Localisation, Double> determinerChronologieDeLaCrue(
+      long[] idsOrigin,
+      double vWaterInit,
+      double k) {
 
+    int nombreNoeuds = tableauNoeuds.length;
 
+    double[] tempsArriveeEau = new double[nombreNoeuds];
+    Arrays.fill(tempsArriveeEau, Double.POSITIVE_INFINITY);
 
+    double[] vitesseEau = new double[nombreNoeuds];
+    Arrays.fill(vitesseEau, 0);
 
+    // Dijkstra
+    PriorityQueue<Integer> filePriorite =
+        new PriorityQueue<>(Comparator.comparingDouble(i -> tempsArriveeEau[i]));
 
-
-
-
-
-
-    public Deque<Localisation> trouverCheminDEvacuationLePlusCourt(
-            long idOrigin,
-            long idEvacuation,
-            double vVehicule,
-            Map<Localisation, Double> tFlood) {
-
-        int n = noeuds.length;
-
-        double[] temps = new double[n];
-        Arrays.fill(temps, Double.POSITIVE_INFINITY);
-
-        int[] parent = new int[n];
-        Arrays.fill(parent, -1);
-        // Prise des ids des points de depart et d'arrivée
-        int origine = idToIndex.get(idOrigin);
-        int fin = idToIndex.get(idEvacuation);
-
-        temps[origine] = 0.0;
-
-        // Dijkstra
-
-        PriorityQueue<Integer> pq =
-                new PriorityQueue<>(Comparator.comparingDouble(i -> temps[i]));
-
-        pq.add(origine);
-
-        while (!pq.isEmpty()) {
-
-            int u = pq.poll();
-
-            if (u == fin) break;
-
-            List<Arc> arcs = listeAdjacence.get(noeuds[u].getId());
-
-            for (Arc arc : arcs) {
-
-                int v = idToIndex.get(arc.getPointArrivee().getId());
-                double dist = arc.getDistance();
-                // Calcul du temps
-                double tempsArc = dist / vVehicule;
-                double tArrivee = temps[u] + tempsArc;
-
-                Localisation locV = noeuds[v];
-                Double tFloodV = tFlood.get(locV);
-
-                if (tFloodV != null && tArrivee > tFloodV) {
-                    continue;
-                }
-
-                if (tArrivee < temps[v]) {
-                    temps[v] = tArrivee;
-                    parent[v] = u;
-                    pq.add(v);
-                }
-            }
-        }
-
-        Deque<Localisation> chemin = new ArrayDeque<>();
-
-        if (temps[fin] == Double.POSITIVE_INFINITY) {
-            return chemin;
-        }
-
-        for (int cur = fin; cur != -1; cur = parent[cur]) {
-            chemin.addFirst(noeuds[cur]);
-        }
-
-        return chemin;
+    for (long identifiant : idsOrigin) {
+      int indice = correspondanceIdVersIndice.get(identifiant);
+      tempsArriveeEau[indice] = 0.0;
+      vitesseEau[indice] = vWaterInit;
+      filePriorite.add(indice);
     }
 
+    while (!filePriorite.isEmpty()) {
+      int indiceNoeudCourant = filePriorite.poll();
 
+      List<Arc> arcsDuNoeud = listeAdjacence.get(tableauNoeuds[indiceNoeudCourant].getId());
 
+      for (Arc arcCourant : arcsDuNoeud) {
+
+        int indiceVoisin = correspondanceIdVersIndice.get(arcCourant.getPointArrivee().getId());
+        double distanceArc = arcCourant.getDistance();
+
+        // Calcul de la pente
+
+        double pente = (tableauNoeuds[indiceNoeudCourant].getAltitude()
+            - tableauNoeuds[indiceVoisin].getAltitude()) / distanceArc;
+        // Calcul de la vitesse
+        double nouvelleVitesse = vitesseEau[indiceNoeudCourant] + k * pente;
+        // Si vitesse négative ou nulle, l'eau s'arrête
+        if (nouvelleVitesse <= 0) {
+          continue;
+        }
+
+        double tempsParcours = distanceArc / nouvelleVitesse;
+
+        double nouveauTempsArrivee = tempsArriveeEau[indiceNoeudCourant] + tempsParcours;
+
+        if (nouveauTempsArrivee < tempsArriveeEau[indiceVoisin]) {
+          tempsArriveeEau[indiceVoisin] = nouveauTempsArrivee;
+          vitesseEau[indiceVoisin] = nouvelleVitesse;
+          filePriorite.add(indiceVoisin);
+        }
+      }
+    }
+
+    Localisation[] copieTriee = Arrays.copyOf(tableauNoeuds, nombreNoeuds);
+    // Tri par temps pour la Map
+    Arrays.sort(copieTriee, Comparator.comparingDouble(
+        localisation -> tempsArriveeEau[correspondanceIdVersIndice.get(localisation.getId())]
+    ));
+
+    Map<Localisation, Double> chronologieCrue = new LinkedHashMap<>();
+
+    for (Localisation localisation : copieTriee) {
+      double tempsInondation = tempsArriveeEau[correspondanceIdVersIndice.get(
+          localisation.getId())];
+      if (tempsInondation < Double.POSITIVE_INFINITY) {
+        chronologieCrue.put(localisation, tempsInondation);
+      }
+    }
+
+    return chronologieCrue;
+  }
+
+  public Deque<Localisation> trouverCheminDEvacuationLePlusCourt(
+      long idOrigin,
+      long idEvacuation,
+      double vVehicule,
+      Map<Localisation, Double> tFlood) {
+
+    int nombreNoeuds = tableauNoeuds.length;
+
+    double[] tempsTrajet = new double[nombreNoeuds];
+    Arrays.fill(tempsTrajet, Double.POSITIVE_INFINITY);
+
+    int[] predecesseur = new int[nombreNoeuds];
+    Arrays.fill(predecesseur, -1);
+    // Prise des ids des points de depart et d'arrivée
+    int indiceOrigine = correspondanceIdVersIndice.get(idOrigin);
+    int indiceEvacuation = correspondanceIdVersIndice.get(idEvacuation);
+
+    tempsTrajet[indiceOrigine] = 0.0;
+
+    // Dijkstra
+
+    PriorityQueue<Integer> filePriorite =
+        new PriorityQueue<>(Comparator.comparingDouble(i -> tempsTrajet[i]));
+
+    filePriorite.add(indiceOrigine);
+
+    while (!filePriorite.isEmpty()) {
+
+      int indiceNoeudCourant = filePriorite.poll();
+
+      if (indiceNoeudCourant == indiceEvacuation) {
+        break;
+      }
+
+      List<Arc> arcsDuNoeud = listeAdjacence.get(tableauNoeuds[indiceNoeudCourant].getId());
+
+      for (Arc arcCourant : arcsDuNoeud) {
+
+        int indiceVoisin = correspondanceIdVersIndice.get(arcCourant.getPointArrivee().getId());
+        double distanceArc = arcCourant.getDistance();
+        // Calcul du temps
+        double tempsParcours = distanceArc / vVehicule;
+        double tempsArriveeVoisin = tempsTrajet[indiceNoeudCourant] + tempsParcours;
+
+        Localisation localisationVoisin = tableauNoeuds[indiceVoisin];
+        Double tempsInondationVoisin = tFlood.get(localisationVoisin);
+
+        if (tempsInondationVoisin != null && tempsArriveeVoisin > tempsInondationVoisin) {
+          continue;
+        }
+
+        if (tempsArriveeVoisin < tempsTrajet[indiceVoisin]) {
+          tempsTrajet[indiceVoisin] = tempsArriveeVoisin;
+          predecesseur[indiceVoisin] = indiceNoeudCourant;
+          filePriorite.add(indiceVoisin);
+        }
+      }
+    }
+
+    Deque<Localisation> cheminEvacuation = new ArrayDeque<>();
+
+    if (tempsTrajet[indiceEvacuation] == Double.POSITIVE_INFINITY) {
+      return cheminEvacuation;
+    }
+
+    for (int indiceCourant = indiceEvacuation; indiceCourant != -1;
+        indiceCourant = predecesseur[indiceCourant]) {
+      cheminEvacuation.addFirst(tableauNoeuds[indiceCourant]);
+    }
+
+    return cheminEvacuation;
+  }
 }
